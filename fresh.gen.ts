1deaeed49280ba34278664a1cc5f5b872b0fa5fc
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-11-25 20:47:06 +0900

    🔥 綺麗にした

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,13 +2,12 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from "./routes/_404.tsx";
-import * as $1 from "./routes/_app.tsx";
-import * as $2 from "./routes/api/search.ts";
-import * as $3 from "./routes/api/tags.ts";
-import * as $4 from "./routes/greet/[name].tsx";
-import * as $5 from "./routes/index.tsx";
-import * as $$0 from "./islands/Counter.tsx";
+import * as $0 from "~/routes/_404.tsx";
+import * as $1 from "~/routes/_app.tsx";
+import * as $2 from "~/routes/api/search.ts";
+import * as $3 from "~/routes/api/tags.ts";
+import * as $4 from "~/routes/index.tsx";
+import * as $$0 from "~/islands/Counter.tsx";
 
 const manifest = {
   routes: {
@@ -16,8 +15,7 @@ const manifest = {
     "./routes/_app.tsx": $1,
     "./routes/api/search.ts": $2,
     "./routes/api/tags.ts": $3,
-    "./routes/greet/[name].tsx": $4,
-    "./routes/index.tsx": $5,
+    "./routes/index.tsx": $4,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
