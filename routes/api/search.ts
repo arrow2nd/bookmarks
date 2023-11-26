3f44b45fa0bf2c17eafd5f46c9332c9c2d5f70fd
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-11-26 17:23:49 +0900

    👍 検索対象を拡大

--- routes/api/search.ts
+++ routes/api/search.ts
@@ -10,27 +10,28 @@ export const handler = (req: Request, _ctx: HandlerContext): Response => {
     return new Response("Method not allowed", { status: 405 });
   }
 
-  const paramas = new URL(req.url).searchParams;
+  const params = new URL(req.url).searchParams;
 
-  const tag = paramas.get("tag")?.toString();
-  const q = paramas.get("q")?.toString();
-
-  // クエリ指定なしなら全件返す
-  if (!q) {
-    return new Response(JSON.stringify(bookmarks));
-  }
+  const tag = params.get("tag")?.toString() || "all";
+  const q = params.get("q")?.toString();
 
   const results = bookmarks.filter((b) => {
     // タグ指定で絞り込み
-    if (tag && b.tag !== tag) {
+    if (tag !== "all" && b.tag !== tag) {
       return false;
+    }
+
+    if (!q) {
+      return true;
     }
 
     // 簡易ファジー検索
     const pattern = q.split("").join(".*");
     const regex = new RegExp(pattern, "i");
 
-    return regex.test(b.title);
+    // タイトル or 説明文にマッチするか
+    return regex.test(b.title) ||
+      (b.description && regex.test(b.description || ""));
   });
 
   return new Response(JSON.stringify(results));
