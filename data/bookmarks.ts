1fa9194237f91fdb3150ca6d78954f02195f918d
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-11-26 15:18:24 +0900

    🔖 タグを修正

--- data/bookmarks.ts
+++ data/bookmarks.ts
@@ -11,7 +11,8 @@ export const bookmarks: Bookmark[] = [{
   "id": "137e4f5fc14f2ac77d0dcad3c99d33642d6fe3dd47b8c9d314fb3f110ec0ae8f",
   "title": "ShinyPoems | シャニマス衣装ポエム検索",
   "url": "https://shiny-poems.vercel.app/",
-  "tag": "その他",
+  "tag": "imas",
   "description": "シャイニーカラーズの衣装ポエムが検索できるサイトです",
   "color": "#4c7abe",
 }];
+
