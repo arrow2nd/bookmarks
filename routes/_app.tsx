62910e88be7654cbed5dbdb67258329b81d8d290
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-12-19 22:09:52 +0900

    👍 サイトタイトルを変更

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component }: AppProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>🔖 Bookmarks</title>
+        <title>Bookmarks</title>
       </head>
       <body>
         <main class="flex justify-center items-center px-6 py-12 min-h-screen bg-stone-100">
