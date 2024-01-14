52e1da3c89c193df4adcc4d117aad784881933e2
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-01-14 21:54:05 +0900

    ♻️ 非推奨になった型に対応

--- routes/api/tags.ts
+++ routes/api/tags.ts
@@ -1,11 +1,11 @@
-import { HandlerContext } from "$fresh/server.ts";
+import { FreshContext } from "$fresh/server.ts";
 import { bookmarks } from "~/data/bookmarks.ts";
 
 /**
  * タグ一覧
  * GET /api/tags
  */
-export const handler = (req: Request, _ctx: HandlerContext): Response => {
+export const handler = (req: Request, _ctx: FreshContext): Response => {
   if (req.method !== "GET") {
     return new Response("Method not allowed", { status: 405 });
   }
