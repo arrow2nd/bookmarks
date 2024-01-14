1c6842fce0bf642c361ea7646c308f0b0b88061d
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-01-14 22:26:25 +0900

    💄 追加モーダルのデザインを変更

--- islands/NewModalForm.tsx
+++ islands/NewModalForm.tsx
@@ -1,5 +1,6 @@
 import { useSignal } from "@preact/signals";
 import { createAddIssueURL } from "~/libs/gh.ts";
+import IconBrandGithub from "tabler-icons/brand-github.tsx";
 import IconBookmarkEdit from "tabler-icons/bookmark-edit.tsx";
 import IconArrowBack from "tabler-icons/arrow-back.tsx";
 
@@ -19,7 +20,7 @@ export default function NewFormModal({ options }: NewFormProps) {
           <div class="fixed top-0 left-0 flex justify-center items-center w-screen h-screen">
             <div class="fixed w-screen h-screen bg-black opacity-50" />
             <form
-              class="md:w-1/2 mx-4 p-4 md:p-6 space-y-2 bg-white rounded-md shadow-2xl animate-fade-in z-10"
+              class="md:w-1/2 mx-4 p-4 md:p-6 bg-white rounded-md shadow-2xl animate-fade-in z-10"
               onSubmit={(e) => {
                 e.preventDefault();
                 window.location.href = createAddIssueURL(
@@ -28,21 +29,33 @@ export default function NewFormModal({ options }: NewFormProps) {
                 );
               }}
             >
-              <select
-                class="block bg-white"
-                onChange={(e) => {
-                  tag.value = e.currentTarget.value;
-                }}
-              >
-                {options.map((t) => <option value={t}>{t}</option>)}
-              </select>
+              <div class="flex">
+                <label class="text-sm text-gray-500">ラベル :</label>
+                <select
+                  class="block bg-white"
+                  onChange={(e) => {
+                    tag.value = e.currentTarget.value;
+                  }}
+                >
+                  {options.map((t) => <option value={t}>{t}</option>)}
+                </select>
+              </div>
               <input
-                class="w-full text-4xl md:text-5xl bg-white focus:outline-none"
+                class="w-full mt-2 text-4xl md:text-5xl bg-white focus:outline-none"
                 placeholder="URLを入力"
                 onInput={(e) => {
                   url.value = e.currentTarget.value;
                 }}
               />
+              <div class="mt-2 text-right">
+                <button
+                  type="submit"
+                  class="inline-flex items-center px-4 py-2 text-sm rounded-lg text-stone-100 bg-gray-800 hover:bg-gray-500 transition-colors"
+                >
+                  <IconBrandGithub class="mr-1 w-5 h-5" />
+                  ブックマークを追加
+                </button>
+              </div>
             </form>
           </div>
         )}
